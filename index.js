1c7ee6dd3ce4ddea9a2b66c058054ba06926abcb
Kanapitch Khamjorn <save@Saves-MacBook-Pro.local>
2024-01-06 22:16:39 +0700

    Added comment to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import dependencies
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
@@ -5,13 +6,17 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+// APIs
 const inspireAPI_URL = "https://zenquotes.io/api/random";
 const programmerAPI_URL = "https://programming-quotesapi.vercel.app/api/random";
 const memeAPI_URL = "https://icanhazdadjoke.com";
 
+// body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+// express middleware to serve static file
 app.use(express.static("public"));
 
+// get default quote once the user enter the website
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get(inspireAPI_URL);
@@ -28,13 +33,14 @@ app.get("/", async (req, res) => {
     }
 });
 
+// post quote depends on the user's choice
 app.post("/", async (req, res) => {
-    const choice = req.body.type;
+    const choice = req.body.type;   // quote choice that user picked
     let data;
     let result = "";
     console.log(choice);
     try {
-        switch (choice) {
+        switch (choice) {   // switch to get proper API
             case "inspiration":
                 result = await axios.get(inspireAPI_URL);
                 data = {
@@ -70,13 +76,14 @@ app.post("/", async (req, res) => {
                 res.render("index.ejs", {error: "Invalid choice. Please try again."})
         }
         console.log(data);
-    } catch (error) {
+    } catch (error) {   // error handling
         console.log("Failed to make request: ", error.message);
         res.render("index.ejs", {error: error.message});
     }
     
 });
 
+// server run local on port 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
